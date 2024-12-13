99dcc45495508c5d43658186aa529da8ca3de127
Luiz <guga.santos0810@gmail.com>
2024-12-13 14:49:48 -0300

    update

--- main.js
+++ main.js
@@ -9,6 +9,24 @@ let cells = [];
 let direction = "right";
 let runningState = true;
 let velocity = 750;
+let apple;
+let gameover = document.getElementById("gameover");
+let interval;
+let score = 0;
+let restart = gameover.querySelector("button");
+
+let firstColumn = [];
+let lastColumn = [];
+for (let y = 0; y < height; y++) {
+  let index = searchTable(y, 0);
+  firstColumn.push(index);
+}
+for (let y = 0; y < height; y++) {
+  let index = searchTable(y, height - 1);
+  lastColumn.push(index);
+}
+
+restart.onclick = restartFunction;
 
 function searchTable(row, col) {
   return row * width + col;
@@ -34,12 +52,17 @@ function drawTable() {
 }
 
 function updateScreen() {
-  area.children[0].innerText = `Direção: ${direction}`;
+  area.children[0].innerText = `Direction: ${direction}`;
+  area.children[1].innerText = `Score: ${score}`;
+  area.children[2].innerText = `Snake: ${snake}`;
+  area.children[3].innerText = `Velocity: ${1000 - velocity}`;
 
   let index = 0;
   cells.forEach((td) => {
-    if (index == snake) {
+    if (snake.includes(index)) {
       td.style.background = "blue";
+    } else if (index == apple) {
+      td.style.background = "red";
     } else {
       td.style.background = "green";
     }
@@ -67,34 +90,90 @@ function getInput() {
   });
 }
 
+function shiftValues() {
+  for (let x = snake.length - 1; x > 0; x--) {
+    snake[x] = snake[x - 1];
+  }
+}
+
 function updateSnake() {
+  shiftValues();
+
   switch (direction) {
     case "up":
-      snake[0] -= height;
+      let upValue = snake[0] - height;
+      if (upValue < 0 || snake.includes(upValue)) gameOver();
+      else snake[0] = upValue;
       break;
     case "down":
-      snake[0] += height;
+      let downValue = snake[0] + height;
+      if (downValue > numberOfCells - 1 || snake.includes(downValue))
+        gameOver();
+      else snake[0] = downValue;
       break;
     case "right":
-      snake[0] += 1;
+      let rightValue = snake[0] + 1;
+      if (lastColumn.includes(rightValue - 1) || snake.includes(rightValue))
+        gameOver();
+      else snake[0] = rightValue;
       break;
     case "left":
-      snake[0] -= 1;
+      let leftValue = snake[0] - 1;
+      if (firstColumn.includes(leftValue + 1) || snake.includes(leftValue))
+        gameOver();
+      else snake[0] = leftValue;
       break;
   }
 
   if (runningState) setTimeout(updateSnake, velocity);
 }
 
+function generateRandomApple() {
+  let randInt = Math.floor(Math.random() * numberOfCells);
+
+  if (snake.includes(randInt)) generateRandomApple();
+  else apple = randInt;
+}
+
+function addScore() {
+  if (snake[0] == apple) {
+    score++;
+    generateRandomApple();
+    snake.push(snake[0]);
+    let newVel = Math.min(score * 10, 30);
+    velocity = Math.max(velocity - newVel, 70);
+  }
+}
+
+function gameOver() {
+  clearInterval(interval);
+  runningState = !runningState;
+  gameover.classList.remove("invisible");
+  gameover.querySelector("p").innerText = `Your score: ${score}`;
+}
+
+function restartFunction() {
+  velocity = 750;
+  runningState = !runningState;
+  gameover.classList.add("invisible");
+  interval = setInterval(loop, 150);
+  setTimeout(updateSnake, velocity);
+  snake = [0];
+  direction = "right";
+  score = 0;
+}
+
 function start() {
   getInput();
   drawTable();
+  generateRandomApple();
   setTimeout(updateSnake, velocity);
+  interval = setInterval(loop, 150);
 }
 
 function loop() {
+  addScore();
   updateScreen();
 }
 
 start();
-setInterval(loop, 150);
